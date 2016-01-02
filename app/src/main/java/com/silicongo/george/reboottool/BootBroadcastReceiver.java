42e0964da42aced0003dfc625bff02d8fdae6b08
suxiaocheng <suxiaocheng2010@gmail.com>
2016-01-02 10:25:17 +0800

    add the wake lock permission

--- app/src/main/java/com/silicongo/george/reboottool/BootBroadcastReceiver.java
+++ app/src/main/java/com/silicongo/george/reboottool/BootBroadcastReceiver.java
@@ -3,6 +3,7 @@ package com.silicongo.george.reboottool;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.PowerManager;
 
 /**
  * Created by suxch on 2015/12/30.
@@ -13,6 +14,10 @@ public class BootBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(action_boot)){
+            PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+            PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, action_boot);
+            wl.acquire(60000);
+
             Intent ootStartIntent=new Intent(context,MainActivity.class);
             ootStartIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(ootStartIntent);
